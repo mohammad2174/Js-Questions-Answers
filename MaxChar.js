64eaa99d3ae403e84e21d6ea5c82a68c5b175d82
mohammad2174 <mohammadreza.khorrami21@gmail.com>
2021-11-29 11:33:25 +0330

    Q & A part-14

---
+++ MaxChar.js
@@ -0,0 +1,26 @@
+// "c"
+// "1"
+const maxChar = (str) => {
+    const charMap = {};
+    let max = 0;
+    let maxChar = '';
+
+    for (let char of str) {
+        if (charMap[char]) {
+            charMap[char]++;
+        } else {
+            charMap[char] = 1;
+        }
+    }
+
+    for (let char in charMap) {
+        if (charMap[char] > max) {
+            max = charMap[char];
+            maxChar = char;
+        }
+    }
+    return maxChar;
+}
+
+console.log(maxChar('abccccccccccd'));
+console.log(maxChar('1231111111111'));
