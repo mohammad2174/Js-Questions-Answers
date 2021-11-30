0e1a2a3f9fd2c3075581107e8379166844ba2e71
mohammad2174 <mohammadreza.khorrami21@gmail.com>
2021-11-30 14:10:55 +0330

    Q & A part-22

---
+++ Fib.js
@@ -0,0 +1,10 @@
+const fib = (n) => {
+    let result = [0, 1];
+
+    for (let i = 2; i <= n; i++) {
+        result.push(result[i - 1] + result[i - 2]);
+    }
+    return result;
+}
+
+console.log(fib(9)); // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
