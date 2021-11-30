6f2d70912caf1c3b5735f97757b023fdde7ebdea
mohammad2174 <mohammadreza.khorrami21@gmail.com>
2021-11-30 13:30:48 +0330

    Q & A part-21 - Another Solution

--- Vowels.js
+++ Vowels.js
@@ -1,13 +1,20 @@
-const vowels = (str) => {
-    let counter = 0;
-    const checker = ['a', 'e', 'i', 'o', 'u'];
+// const vowels = (str) => {
+//     let counter = 0;
+//     const checker = ['a', 'e', 'i', 'o', 'u'];
 
-    for (const char of str.toLowerCase()) {
-        if (checker.includes(char)) {
-            counter++;
-        }
-    }
-    return counter;
+//     for (const char of str.toLowerCase()) {
+//         if (checker.includes(char)) {
+//             counter++;
+//         }
+//     }
+//     return counter;
+// }
+
+/*           Another Solution           */
+
+const vowels = (str) => {
+    const matches = str.match(/[aeiou]/gi);
+    return matches ? matches.length : 0;
 }
 
 console.log(vowels('Hi There!')); // 3
